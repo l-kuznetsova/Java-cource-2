d7aa66e1037ea45233e0f7652c59bca598dbb940
lkuzn <l.kuznetsova95@mail.ru>
2021-12-13 20:32:00 +0400

    Init commit

---
+++ src/Main.java
@@ -0,0 +1,76 @@
+public class Main {
+    public static void main(String[] args) {
+        //Задание 1//
+        String town = "Москва";
+        int Population = 5000000;
+        double unemployed = 13.3;
+
+        System.out.println("Город: " + town + " Население: " +Population + " Процент безработных: " + unemployed + "%");
+
+        //Задание 2//
+        char section = 'a';
+        int seat = 125;
+        double sum = 565.50;
+
+        System.out.println("Секция: " + section + " Место: " +seat + " Стоимость билета: " + sum + " рублей");
+
+        //Задание 3//
+        String name = "Елена";
+        byte age = 26;
+        int height = 167;
+
+        System.out.println("Имя: " + name + " Возраст: " +age + " Рост: " + height + " см");
+
+        //Задание 4//
+        String number1 = "У";
+        int number2 = 819;
+        String number3 = "ОН";
+        int number4 = 799;
+        double weight = 2.5;
+
+        System.out.println("Регистрационный номер автомобиля: " + number1 + number2 + number3 + number4 + " Вес автомобиля: " +weight +" тонны");
+
+        //Задание 5//
+        String company = "VTB";
+        int income = 50000000;
+        double share = 15.5;
+
+        System.out.println("Наименование компании: " + company + " Годовой доход: " +income +" рублей " + " Доля на рынке: " + share + "%");
+
+        //Задание 6//
+        String rhesus = "Положительный";
+        byte type = 1;
+        double people = 85.5;
+
+        System.out.println("Резус фактор: " + rhesus + " Группа крови: " +type + " Доля людей с этой группой крови: " + people + "%");
+
+        //Задание 7//
+        String university = "ПГСГА";
+        int students = 30000;
+        double deducted = 5.2;
+
+        System.out.println("Название университета: " + university + " Число студентов: " +students + " Доля отчисленных: " + deducted + "%");
+
+        //Задание 8//
+        byte ratingFilm = 8;
+        int copies = 5586565;
+        byte rating = 7;
+
+        System.out.println("Рейтинг фильма: " + ratingFilm + " Продано копий: " +copies + " Рейтинг: " + rating);
+
+        //Задание 9//
+        String brand = "Adidas";
+        int product = 88335;
+        double sales = 73.3;
+
+        System.out.println("Название бренда: " + brand + " Произведено товара: " +product + " Процент продаж: " + sales + "%");
+
+        //Задание 10//
+        String category = "Одежда";
+        int code = 455664565;
+        double buyers30 = 35.3;
+
+        System.out.println("Категория товара: " + category + " Штрих код: " +code + " Процент покупателей старше 30: " + buyers30 + "%");
+
+    }
+}
